b1b722789af17e831e5d4c5bfd6e7e4e050b89dc
tanisha316 <tanishaten@gmail.com>
2025-12-01 11:24:43 +0530

    Add files via upload

---
+++ TicTacToe.java
@@ -0,0 +1,84 @@
+import java.util.Scanner;
+public class TicTacToe {
+    static char[][] board = {
+        {' ', ' ', ' '},
+        {' ', ' ', ' '},
+        {' ', ' ', ' '}
+    };
+
+    public static void printBoard() {
+        System.out.println("-------------");
+        for (int i = 0; i < 3; i++) {
+            System.out.print("| ");
+            for (int j = 0; j < 3; j++) {
+                System.out.print(board[i][j] + " | ");
+            }
+            System.out.println();
+            System.out.println("-------------");
+        }
+    }
+
+    public static boolean isWinner(char player) {
+        // check rows
+        for (int i = 0; i < 3; i++) {
+            if (board[i][0] == player && board[i][1] == player && board[i][2] == player)
+                return true;
+        }
+        // check cols
+        for (int j = 0; j < 3; j++) {
+            if (board[0][j] == player && board[1][j] == player && board[2][j] == player)
+                return true;
+        }
+        // check diagonals
+        if (board[0][0] == player && board[1][1] == player && board[2][2] == player)
+            return true;
+        if (board[0][2] == player && board[1][1] == player && board[2][0] == player)
+            return true;
+
+        return false;
+    }
+
+    public static boolean isBoardFull() {
+        for (int i = 0; i < 3; i++)
+            for (int j = 0; j < 3; j++)
+                if (board[i][j] == ' ')
+                    return false;
+        return true;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        char currentPlayer = 'X';
+        boolean gameEnded = false;
+
+        System.out.println("Welcome to Tic Tac Toe!");
+        printBoard();
+
+        while (!gameEnded) {
+            System.out.println("Player " + currentPlayer + ", enter your move (row[1-3] and col[1-3]): ");
+            int row = sc.nextInt() - 1;
+            int col = sc.nextInt() - 1;
+
+            if (row < 0 || col < 0 || row > 2 || col > 2) {
+                System.out.println("This position is off the bounds! Try again.");
+            } else if (board[row][col] != ' ') {
+                System.out.println("This position is already taken! Try again.");
+            } else {
+                board[row][col] = currentPlayer;
+                printBoard();
+
+                if (isWinner(currentPlayer)) {
+                    System.out.println("Player " + currentPlayer + " wins!");
+                    gameEnded = true;
+                } else if (isBoardFull()) {
+                    System.out.println("It's a draw!");
+                    gameEnded = true;
+                } else {
+                    currentPlayer = (currentPlayer == 'X') ? 'O' : 'X';
+                }
+            }
+        }
+
+        sc.close();
+    }
+}
